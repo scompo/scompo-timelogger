0e825ef363fd326332fc01053dacb53cd654489e
Mauro Scomparin <scompo@gmail.com>
2018-01-24 19:57:45 +0100

    Mocking needed properties.

--- src/test/java/tk/scompo/timelogger/webapp/TimeloggerWebappTest.java
+++ src/test/java/tk/scompo/timelogger/webapp/TimeloggerWebappTest.java
@@ -1,9 +1,9 @@
 package tk.scompo.timelogger.webapp;
 
 import static org.hamcrest.Matchers.containsString;
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.*;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -12,6 +12,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.RequestPostProcessor;
@@ -20,6 +21,7 @@ import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@TestPropertySource(properties = { "security.oauth2.client.clientId=cl", "security.oauth2.client.clientSecret=sec" })
 public class TimeloggerWebappTest {
 
 	@Autowired
