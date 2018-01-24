7232ab2a5ddf8e254730da9ca3f958a8b34961d4
Mauro Scomparin <scompo@gmail.com>
2018-01-24 19:37:05 +0100

    Fixed test security configuration.

--- src/test/java/tk/scompo/timelogger/webapp/TimeloggerWebappTest.java
+++ src/test/java/tk/scompo/timelogger/webapp/TimeloggerWebappTest.java
@@ -1,28 +1,57 @@
 package tk.scompo.timelogger.webapp;
 
-import org.junit.*;
-import org.junit.runner.*;
-import org.springframework.beans.factory.annotation.*;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.RequestPostProcessor;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest
+@SpringBootTest
 public class TimeloggerWebappTest {
 
 	@Autowired
+	private WebApplicationContext wac;
+
 	private MockMvc mvc;
 
+	@Before
+	public void setup() {
+		mvc = MockMvcBuilders.webAppContextSetup(wac).apply(springSecurity()).build();
+	}
+
 	@Test
-	@WithMockUser(roles = "USER")
 	public void testMain() throws Exception {
 		this.mvc.perform(get("/")).andExpect(status().isOk())
 				.andExpect(content().string(containsString("<h1>scompo-timelogger webapp</h1>")));
+	}
+
+	@Test
+	public void testConfigUnauthorized() throws Exception {
+		this.mvc.perform(get("/config")).andExpect(status().isForbidden());
+	}
+
+	@Test
+	public void testConfigAutorized() throws Exception {
+		this.mvc.perform(get("/config").with(testUser())).andExpect(status().isOk())
+				.andExpect(content().string(containsString("<title>scompo-timelogger webapp - config</title>")));
+	}
+
+	private static RequestPostProcessor testUser() {
+		return user("testUser").roles("USER");
 	}
 }
